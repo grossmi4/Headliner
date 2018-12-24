c2b5f1a43cfef79a80c6e267d1a00229557e645d
Mike Gross <grossmi4@gmail.com>
2018-12-24 09:29:17 -0500

    added basic scraping logic - console logging description

---
+++ controllers/scraperController.js
@@ -0,0 +1,21 @@
+const db = require("../models"); //get models for db
+const axios = require("axios"); //used to make get requests for scraping
+const cheerio = require("cheerio"); //used to parse html from scraping requests
+
+module.exports = function(app) {
+  //get top 5 new stories
+  app.get("/", function(req, res) {
+    //use axios to retrieve html from reuters.com
+    axios.get("http://www.reuters.com/").then(function(response) {
+      //load html into cheerio
+      const $ = cheerio.load(response.data);
+
+      //grabs the first article tags with class 'story'
+      $("article.story").each(function(i, element) {
+        const description = $(element).find("p").val();
+        console.log(description)
+      })
+    })
+  });
+
+}
