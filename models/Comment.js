9b7de58742b3880a4319ccb1494b3de9f2ec8243
Mike Gross <grossmi4@gmail.com>
2018-12-24 10:09:29 -0500

    Added comment model and index model

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    required: true
+  }
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
