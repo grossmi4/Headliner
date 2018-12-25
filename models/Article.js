3ad433854e5cd4299b86a706ab3b40cfd4f47f3b
Mike Gross <grossmi4@gmail.com>
2018-12-25 12:40:17 -0500

    improved comments

--- models/Article.js
+++ models/Article.js
@@ -4,27 +4,33 @@ const Comment = require("./Comment");
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
+  // headline is uniquely defined. this prevents multiple additions to mongodb of the same article
   headline: {
     type: String,
     required: true,
     unique: true
   },
+  // secondary information displayed below headline to give better glimpse
   description: {
     type: String,
     required: true
   },
+  // for informational purposes only
   dateRetrieved: {
     type: Date,
     required: true,
     default: Date.now()
   },
+  // link to article on Reuters.com
   articleLink: {
     type: String,
     required: true
   },
+  // link to image associated with artcile on reuters
   imgLink: {
     type: String
   },
+  // array with relationship to Comment.js model file
   comments: [
     {
       type: Schema.Types.ObjectId,
@@ -33,6 +39,7 @@ const ArticleSchema = new Schema({
   ]
 });
 
+// export model
 const Article = mongoose.model("Article", ArticleSchema);
 
 module.exports = Article;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+// index file to allow mapping to entire models folder in one require statement. see server.js for example.
+
 module.exports = {
   Article: require("./Article"),
   Comment: require("./Comment")
